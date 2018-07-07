6dcccde8eea3931c1f5702eebb229aa2ac15d103
andysnake <andrea.diiorio.sniper@gmail.com>
2018-07-07 19:13:02 +0200

    grafica base inserita

---
+++ src/GUI/MainMenuController.java
@@ -0,0 +1,78 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class MainMenuController {
+    //logged to acces this controller
+    @FXML
+    Button PrenotazioneConId;
+    @FXML
+    Button PrenotazioneConCaratteristiche;
+    @FXML
+    Button loggout;
+    @FXML
+    Button visualizzaPrenotazioniAttive;
+    @FXML
+    Text text;
+    //andysnake buttons
+    @FXML
+    Button visualizzaDisponibilita;
+    @FXML
+    Button visualizzaPrenotazioniAttiveTot;
+
+    @FXML
+    Button definizioneSessione;
+
+    @FXML
+    Button storicoPrenotazioni;
+
+    @FXML
+    protected void loggout(ActionEvent event) throws Exception {
+        //todo destroy possible boundaries
+        //...
+        //swap view (fxml)
+
+        ViewSwap.getIstance().swap(event, ViewSwap.LOGGIN);
+    }
+
+    @FXML
+    protected void PrenotazioneConId(ActionEvent event) throws Exception {
+        //todo GOTO PRENOTAZIONE CON ID CONTROLLER.... SUBTIPE OF SPECIFICACARAULACONTROLLER?
+            text.setText("AZIONE NON CONSENTITA ALL'UTENTE NORMALE");
+
+    }
+
+
+    @FXML
+    protected void prenotazioneConCaratteristiche(ActionEvent event) throws Exception {
+        }
+
+    @FXML
+    protected void visualizzaDisponibilita(ActionEvent event) throws Exception {
+    }
+
+    @FXML
+    protected void visualizzaPrenotazioniAttiveToT(ActionEvent event) throws Exception {
+    }
+
+    @FXML
+    protected void visualizzaPrenotazioniAttive(ActionEvent event) throws Exception {
+    }
+
+    @FXML
+    protected void apriUnAnnoAccademico(ActionEvent event) throws Exception {
+        }
+
+    @FXML
+    protected void visualizzaStorico(ActionEvent event) throws Exception {
+    }
+
+    @FXML
+    protected void definisciSessione(ActionEvent event) throws Exception {
+    }
+
+
+}
